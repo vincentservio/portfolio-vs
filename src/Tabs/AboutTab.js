a34dbfce9d29b861ffcd7314a2581256ac18ff39
vincentservio <vincentservio@gmail.com>
2021-04-18 23:20:09 -0400

    timeline code

--- src/Tabs/AboutTab.js
+++ src/Tabs/AboutTab.js
@@ -27,6 +27,64 @@ function AboutTab() {
       <center>
         <Languages></Languages>
       </center>
+      <div class="cardTop">
+        <svg
+          width="497"
+          height="219"
+          viewBox="0 0 497 219"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M-38.5 196C-38.5 196 34 91 133.5 91C233 91 427 159 532.5 30C638 -99 518 236 518 236L-49 246.5L-38.5 196Z"
+            fill="#FF768E"
+          />
+        </svg>
+      </div>
+      <div class="timeline">
+        <h3>Updates</h3>
+        <label>23 in the last 7 hours</label>
+
+        <div class="box">
+          <div class="container">
+            <div class="lines">
+              <div class="dot"></div>
+              <div class="line"></div>
+              <div class="dot"></div>
+              <div class="line"></div>
+              <div class="dot"></div>
+              <div class="line"></div>
+            </div>
+
+            <div class="cards">
+              <div class="card">
+                <h4>16:30</h4>
+                <p>
+                  Believing Is The Absence
+                  <br /> Of Doubt
+                </p>
+              </div>
+              <div class="card mid">
+                <h4>15:22</h4>
+                <p>Start With A Baseline</p>
+              </div>
+              <div class="card">
+                <h4>14:15</h4>
+                <p>
+                  Break Through Self Doubt
+                  <br /> And Fear
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="bottom">
+            <div class="btn">Show all updates</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
